8188f439b79493a565c52047903e086d7ad7ee6a
Jason <jason.morris1813@gmail.com>
2022-09-12 22:50:34 -0500

    Get everything working right with express

--- prisma-api/index.ts
+++ prisma-api/index.ts
@@ -1,40 +1,51 @@
 
 import { PrismaClient } from '@prisma/client'
+import express from 'express'
 
 const prisma = new PrismaClient()
+const app = express()
 
-async function main() {
-    // const allUsers = await prisma.user.findMany()
-    // console.log(allUsers)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-    await prisma.user.create({
-        data: {
-          username: 'buckeyemania23',
-          password: 'cubs',
-          threads: {
-            create: { 
-                // createdAt: 2020-03-21T16:45:01.246Z,
-                title: 'hello'
-            },
-          }
-        },
-      })
+// async function main() {
+//     // const allUsers = await prisma.user.findMany()
+//     // console.log(allUsers)
 
-      const allUsers = await prisma.user.findMany({
-        include: {
-          threads: true,
-        //   profile: true,
-        },
-      })
-      console.dir(allUsers, { depth: null })
-}
+//     await prisma.user.create({
+//         data: {
+//           username: 'buckeyemania23',
+//           password: 'cubs',
+//           threads: {
+//             create: { 
+//                 // createdAt: 2020-03-21T16:45:01.246Z,
+//                 title: 'hello'
+//             },
+//           }
+//         },
+//       })
 
-main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+//       const allUsers = await prisma.user.findMany({
+//         include: {
+//           threads: true,
+//         //   profile: true,
+//         },
+//       })
+//       console.dir(allUsers, { depth: null })
+// }
+
+// main()
+//     .then(async () => {
+//         await prisma.$disconnect()
+//     })
+//     .catch(async (e) => {
+//         console.error(e)
+//         await prisma.$disconnect()
+//         process.exit(1)
+//     })
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`🚀 Server ready at: http://localhost:${port}`)
+})
